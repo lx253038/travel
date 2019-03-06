234247c108e8fcb75e7061fe7325511129039cbd
lixin1sj <lixin1sj@yonyou.com>
2019-03-06 21:16:12 +0800

    字母表滑动使得城市列表滚动

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -20,7 +20,7 @@
         </div>
       </div>
       <!-- 字母城市 -->
-      <div class="area" v-for="(item,key) of cities" :key="key">
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom" v-for="city of item" :key="city.id">{{city.name}}</div>
@@ -38,7 +38,16 @@ export default {
   },
   props: {
     hotCities: Array,
-    cities: Object
+    cities: Object,
+    letter: String
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
